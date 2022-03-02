06c4a23f75b4e294487a94670e8ea54432c7e57f
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-03-02 22:45:13 +0900

    main image

--- src/components/StarBucks.js
+++ src/components/StarBucks.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Nav from "./Nav";
+import "./StarBucks.scss";
 
 class StarBucks extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <img
+          className="main"
+          src="https://athome.starbucks.com/sites/default/files/styles/homepage_banner_xlarge/public/2022-03/CAH_Header_Assets_Homepage_Header_2880x1660_B.jpg?h=cf77c377&itok=ipEM8Tm3"
+          alt=""
+        />
       </div>
     );
   }
