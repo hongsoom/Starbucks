de0549efc718019f6ef52d8e6a0366fa53625453
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-03-09 03:53:10 +0900

    Main page

--- src/View/Main/main.js
+++ src/View/Main/main.js
@@ -1,15 +1,50 @@
 import React, { Component } from "react";
+import COFFEE from "../Coffee/COFFEE";
+import MENU from "../Menu/MenuMain";
+import LOGIN from "../Login/Login";
 import "./main.scss";
 
 class Main extends Component {
   render() {
     return (
-      <div className="main">
-        <img
-          className="images"
-          src="https://athome.starbucks.com/sites/default/files/styles/homepage_banner_large/public/2022-02/Chai_Latte_Web_New_Header_2880x1660.jpg?h=cf77c377&itok=rJgzT1Cm"
-          alt="Caramel Dirty Chai Latte"
-        />
+      <div>
+        <div className="main">
+          <div className="maininner">
+            <div class="main-visual_slogan">
+              <img
+                src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Spring_main_slogan.png"
+                alt="BRING NEW SPRING"
+                class="pc-slogan"
+              />
+            </div>
+            <div class="main-visual_set">
+              <div class="set_common set_01">
+                <img
+                  src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Spring_main_ChouxCreamLatte.png"
+                  alt="슈크림 라떼"
+                  class="pc-drink"
+                />
+              </div>
+              <div class="set_common set_02">
+                <img
+                  src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Spring_main_PinkMagnolia.png"
+                  alt="핑크 매그놀리아 레모네이드"
+                  class="pc-drink"
+                />
+              </div>
+              <div class="set_common set_03">
+                <img
+                  src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Spring_main_GreenYogurt.png"
+                  alt="파인 코코 그린 요거트 블렌디드"
+                  class="pc-drink"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <COFFEE />
+        <MENU />
+        <LOGIN />
       </div>
     );
   }
