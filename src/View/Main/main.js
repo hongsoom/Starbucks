b94709f34e67d54951d3bb829a8a533201c41fbe
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-03-09 15:59:33 +0900

    Modify CSS

--- src/View/Main/main.js
+++ src/View/Main/main.js
@@ -8,35 +8,31 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <div className="main">
-          <div className="maininner">
-            <div class="main-visual_slogan">
+        <div className="Main">
+          <div className="MainInner">
+            <div class="MainTitle">
               <img
                 src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Spring_main_slogan.png"
                 alt="BRING NEW SPRING"
-                class="pc-slogan"
               />
             </div>
-            <div class="main-visual_set">
-              <div class="set_common set_01">
+            <div class="MainMenu">
+              <div class="MainMenu1">
                 <img
                   src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Spring_main_ChouxCreamLatte.png"
                   alt="슈크림 라떼"
-                  class="pc-drink"
                 />
               </div>
-              <div class="set_common set_02">
+              <div class="MainMenu2">
                 <img
                   src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Spring_main_PinkMagnolia.png"
                   alt="핑크 매그놀리아 레모네이드"
-                  class="pc-drink"
                 />
               </div>
-              <div class="set_common set_03">
+              <div class="MainMenu3">
                 <img
                   src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Spring_main_GreenYogurt.png"
                   alt="파인 코코 그린 요거트 블렌디드"
-                  class="pc-drink"
                 />
               </div>
             </div>
